6e6036ef6efff79dfe0f59e063e578d43c797865
alu98@naver.com <alu98@naver.com>
2024-02-22 10:37:40 +0900

    ResultMap Association
    association 태그 압축까지 완료

---
+++ chap04-mapper-element-lecture-source/src/main/java/com/ohgiraffers/section01/xmlmapper/Application.java
@@ -0,0 +1,63 @@
+package com.ohgiraffers.section01.xmlmapper;
+
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        ElementService elementService = new ElementService();
+
+        do {
+            System.out.println("========= Mapper elements test Menu =========");
+            System.out.println("1. <resultMap> 테스트");
+//            System.out.println("2. <sql> 테스트");
+//            System.out.println("3. <insert> 태스트");
+            System.out.println("9. 프로그램 종료");
+            System.out.print("메뉴 번호를 입력하세요:");
+            int no = sc.nextInt();
+
+            switch (no) {
+                case 1 :
+                    resultMapSubMenu();
+                    break;
+                case 9 :
+                    return;
+            }
+        } while (true);
+    }
+
+    private static void resultMapSubMenu() {
+        Scanner sc = new Scanner(System.in);
+        ElementService elementService = new ElementService();
+        do {
+            System.out.println("========= <resultMap> SubMenu =========");
+            System.out.println("1. <resultMap> Test");
+            System.out.println("2. <association> Test");
+            System.out.println("3. <collection> Test");
+            System.out.println("9. 이전 메뉴로");
+            System.out.print(" 메뉴 번호를 입력하세요 :");
+            int no = sc.nextInt();
+            /* 필기.
+             *  Mybatis 에서 관계 정의
+             *  ( 사원 , 부서 )
+             *  1. association = 1 : 1  (EmployeeDTO 안에 DepartmentDTO가 1개 있다 -> 1:1 관계 )
+             *  2. collection = 1 : N   (DepartMentDTO 안에 EmployeeDTO가 list 형태로 있다 -> 1: N 관계 )
+            * */
+
+            switch (no) {
+                case 1:
+                    elementService.selectResultMapTest();
+                    break;
+                case 2:
+                    elementService.selectResultMapAssociationTest();
+                    break;
+                case 3:
+                    break;
+                case 9:
+                    return;
+            }
+
+
+        }while (true);
+    }
+}
---
+++ chap04-mapper-element-lecture-source/src/main/java/com/ohgiraffers/section01/xmlmapper/CategoryDTO.java
@@ -0,0 +1,50 @@
+package com.ohgiraffers.section01.xmlmapper;
+
+public class CategoryDTO {
+    private int categoryCode;
+    private String categoryName;
+    private Integer refCategoryCode;
+    // 필기. int 자료형엔 null이 들어갈 수 없지만 Integer는 가능하기 때문에 null값을 넣을 정수형이라면 Integer를 사용하자.
+
+    public CategoryDTO() {
+    }
+
+    public CategoryDTO(int categoryCode, String categoryName, Integer refCategoryCode) {
+        this.categoryCode = categoryCode;
+        this.categoryName = categoryName;
+        this.refCategoryCode = refCategoryCode;
+    }
+
+    public int getCategoryCode() {
+        return categoryCode;
+    }
+
+    public void setCategoryCode(int categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Integer getRefCategoryCode() {
+        return refCategoryCode;
+    }
+
+    public void setRefCategoryCode(Integer refCategoryCode) {
+        this.refCategoryCode = refCategoryCode;
+    }
+
+    @Override
+    public String toString() {
+        return "CategoryDTO{" +
+                "categoryCode=" + categoryCode +
+                ", categoryName='" + categoryName + '\'' +
+                ", refCategoryCode=" + refCategoryCode +
+                '}';
+    }
+}
